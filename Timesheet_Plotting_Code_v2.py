6cf685c6f56993fd4c6fb51c6625741dd0f0ba07
mbebic <mjb@achillearesearch.com>
2022-01-13 10:29:14 -0500

    added second function
    employees_by_project_daterange plots each employee and their hours spent on various projects for a pre-determined time range.

--- Timesheet_Plotting_Code_v2.py
+++ Timesheet_Plotting_Code_v2.py
@@ -34,7 +34,6 @@ def projects_by_employee_daterange(pltPdf1, df1a, axis_title=None, plot_size=(6.
     mindate, maxdate = df1['Day'].agg(['min','max'])
     ix1 = pd.date_range(mindate,maxdate,freq='1D')
     yz = np.zeros(ix1.shape)
-    # df9 = pd.DataFrame({'yz': yz}, index=ix1)
     ds9 = pd.Series(yz, index=ix1)
     
     
@@ -49,13 +48,55 @@ def projects_by_employee_daterange(pltPdf1, df1a, axis_title=None, plot_size=(6.
         
         ax0.bar(x, y, bottom = yz, alpha=0.5)
         ds9.loc[x] += y
-        
             
     if axis_title is not None:
         ax0.set_title(axis_title)
 
     ax0.grid(True)
     ax0.legend(employees, loc = 'upper left')
+    
+    ax0.set_xlim([x2,x1])
+    ax0.set_xlabel('Day')
+    dtFmt = mdates.DateFormatter('%m'+'/'+'%d') # define the formatting
+    ax0.xaxis.set_major_formatter(dtFmt) # apply the format to the desired axis
+    
+    pltPdf1.savefig()
+    plt.close()
+
+    return
+
+def employees_by_project_daterange(pltPdf1, df1a, axis_title=None, plot_size=(6.5,4.8)):
+    fig, (ax0) = plt.subplots(nrows=1, ncols=1, figsize=plot_size, sharex=True)
+    
+    # this reads todays date
+    x1 = datetime.datetime.now()
+    # this reads the date 10 weeks prior to todays date
+    x2 = x1 + datetime.timedelta(weeks = -10)
+    projects = df1a['Project'].unique()
+    
+    mindate, maxdate = df1['Day'].agg(['min','max'])
+    ix1 = pd.date_range(mindate,maxdate,freq='1D')
+    yz = np.zeros(ix1.shape)
+    ds9 = pd.Series(yz, index=ix1)
+    
+    for p in projects:
+
+        df1b = df1a[df1a['Project'] == p]
+        df1c = df1b[(df1b['Day'] >= x2) & (df1b['Day'] <= x1)]
+        df1d = df1c.groupby(by=['Day']).sum() #cumulative sum of the hours worked on project alpha per day
+        
+        x = df1d.index.values
+        y = df1d['Duration'].values #duration of values per day
+        yz = ds9.loc[x]
+        
+        ax0.bar(x, y, bottom = yz, alpha=0.5)
+        ds9.loc[x] += y
+            
+    if axis_title is not None:
+        ax0.set_title(axis_title)
+
+    ax0.grid(True)
+    ax0.legend(projects, loc = 'upper left')
     
     ax0.set_xlim([x2,x1])
     ax0.set_xlabel('Day')
@@ -98,12 +139,19 @@ if __name__ == "__main__":
 
     print('There are '+str(len(employees))+' unique employees and '+str(len(projects))+' unique projects')
             
-    if True:
+    if False:
         
         for p in projects:
             df1a = df1[df1['Project'] == p]
             
             projects_by_employee_daterange(pltPdf1, df1a, axis_title=p)
+            
+    if True:
+        
+        for e in employees:
+            df1a = df1[df1['Employee'] == e]
+            
+            employees_by_project_daterange(pltPdf1, df1a, axis_title=e)
             
 
     print('Closing pdf file')
